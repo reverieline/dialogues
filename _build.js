3dba8ba29a6f8d792aca0b6b10e36116019b4822
Alex Gavr (KORA) <reverieline@gmail.com>
2022-04-17 12:23:22 +0300

    upd

--- _build.js
+++ _build.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const os=require('os');
 
 function buildDir(path){
   const out=[];
@@ -25,6 +26,28 @@ function buildDir(path){
   return out;
 }
 
+function preprocessTextFiles(dir){
+  fs.readdirSync(dir,{withFileTypes:true}).forEach(file => {
+    
+    if(file.isDirectory()){
+      preprocessTextFiles(dir+"/"+file.name);
+    }else if(/\.txt$/gi.test(file.name)){
+      let path=dir+"/"+file.name;
+      console.log(path);
+      let text=fs.readFileSync(path,{encoding:"utf8"}).toString();
+      let outtext="";
+      text.split(/\r?\n/).forEach(function(line){
+        line=line.replace(/\s{2,}/gi,' ');
+        line=line.replace(/\s+$/gi,'');
+        line+=os.EOL;
+        outtext+=line;
+      });
+            
+      fs.writeFileSync(path,outtext);
+    }
+  });
+}
+
+preprocessTextFiles('./_out');
 const out=buildDir('./_out');
-console.log(out);
 fs.writeFileSync('index.json',JSON.stringify(out));
