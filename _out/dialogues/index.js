26e2f1c1c3cd66eaa708d96371c146fb3b306e7d
Alex Gavr (KORA) <reverieline@gmail.com>
2022-04-17 10:45:06 +0300

    upd

--- _out/dialogues/index.js
+++ _out/dialogues/index.js
@@ -9,4 +9,43 @@ module.exports=[
     },
     nb_lines: 3,
   },
+  {
+    type: 'file',
+    path:"000_how_are_you.txt",
+    title:{
+      en: "Quick meet",
+      ru: "Встреча",
+      uk: "Зустріч",
+    },
+    nb_lines: 7,
+  },
+  {
+    type: 'file',
+    path:"001_where_are_you_from.txt",
+    title:{
+      en: "Where do you come from?",
+      ru: "Откуда ты?",
+      uk: "Звідки ти?",
+    },
+    nb_lines: 9,
+  },
+  {
+    type: 'file',
+    path:"002_usual_day.txt",
+    title:{
+      en: "A usual day",
+      ru: "Обычный день",
+      uk: "Звичайний день",
+    },
+    nb_lines: 16,
+  },
+  {
+    type: 'file',
+    path:"003_visiting_the_cinema.txt",
+    title:{
+      en: "Visiting the cinema",
+      ru: "Поход в кино",
+    },
+    nb_lines: 33,
+  },
 ];
